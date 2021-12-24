4e6ce9614ca13617d8bec801be804a27e10d122b
JuniorMira <j-unyor14@hotmail.com>
2021-12-24 14:46:25 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 const button = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
-const lista = document.querySelector('#lista-tarefas');
+const listaOrdenada = document.querySelector('#lista-tarefas');
+const listas = document.getElementsByClassName('li');
+
+/* ------------------------------------------------- */
+
+function clearList() {
+  for (let i = 0; i < listas.length; i += 1) {
+    if (listas[i].classList.contains('selected')) {
+      listas[i].classList.remove('selected');
+    }
+  }
+}
+
+function creatEvent(element, handler) {
+  element.addEventListener('click', handler);
+}
+
+// Cria listas no input
 
 function makeList() {
   const orderList = document.createElement('li');
+  orderList.classList.add('li');
+  creatEvent(orderList, () => {
+    clearList();
+    orderList.classList.add('selected');
+  });
   orderList.innerText = input.value;
-  lista.appendChild(orderList);
+  listaOrdenada.appendChild(orderList);
   input.value = '';
 }
 
-button.addEventListener('click', () => {
-  makeList();
-});
+// seleciona com click cada elemento da lista
+
+window.onload = () => {
+  button.addEventListener('click', () => {
+    makeList();
+  });
+};
